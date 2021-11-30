fb67544c3ec2a99cda6971582027f5326b6016fa
孤独な 宇宙飛行士 <50376577+TheLonelyAstronaut@users.noreply.github.com>
2021-11-30 14:41:30 +0300

    test

--- tools/version-increaser/command-line.ts
+++ tools/version-increaser/command-line.ts
@@ -1,11 +1,27 @@
 import childProcess from 'child_process';
+import { Readable } from 'stream';
+
+export const streamToString = async (stream: Readable | null): Promise<string> => {
+    const chunks: Buffer[] = [];
+
+    if(!stream) {
+        return 'null stream';
+    }
+
+    return new Promise((resolve, reject) => {
+        stream.on('data', (chunk) => chunks.push(Buffer.from(chunk)));
+        stream.on('error', (err) => reject(err));
+        stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf8')));
+    })
+}
 
 export const execute = async (command: string): Promise<string> => {
     const { stdout, stderr } = await childProcess.exec(command);
+    const decoded = await streamToString(stdout ?? stderr);
 
     if(stderr) {
-        console.error(stderr);
+        console.error(decoded);
     }
 
-    return stdout?.read().toString() ?? stderr?.read().toString()!!
+    return decoded;
 }
--- tools/version-increaser/index.ts
+++ tools/version-increaser/index.ts
@@ -1,11 +1,14 @@
 import { version as currentVersion } from '../../package.json';
 import { setVersionNumber } from './set-version-number';
 import { increaseVersionNumber } from './increase-version-number';
+import { execute } from './command-line';
 
 const main = async () => {
     const version = increaseVersionNumber(currentVersion);
 
     await setVersionNumber(version);
+    await execute(`git tag v${version} && git push origin --tags`);
+    await execute(`gh release create v${version} --title "Release v${version}" --notes ""`)
 }
 
 main().catch(console.error);
