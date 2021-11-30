612c6d9525bd14ebb880b4c4494d946eaeda6bfc
孤独な 宇宙飛行士 <50376577+TheLonelyAstronaut@users.noreply.github.com>
2021-11-30 14:19:44 +0300

    very sm0l fix

--- tools/version-increaser/increase-version-number.ts
+++ tools/version-increaser/increase-version-number.ts
@@ -3,5 +3,5 @@ import { getBuildInfo } from "./config";
 export const increaseVersionNumber = (version: string): string => {
     const { buildVersion, buildNumber } = getBuildInfo(version);
 
-    return `${buildVersion}.${buildNumber + 1}`;
+    return `${buildVersion}.${Number(buildNumber) + 1}`
 }
