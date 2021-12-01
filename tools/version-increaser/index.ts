3cb5adee939c8971238893fa8c9be45e81fa2fcf
孤独な 宇宙飛行士 <50376577+TheLonelyAstronaut@users.noreply.github.com>
2021-12-01 13:02:01 +0300

    Pushing to the moon

--- tools/version-increaser/index.ts
+++ tools/version-increaser/index.ts
@@ -3,12 +3,17 @@ import { increaseVersionNumber } from './increase-version-number';
 import { execute } from './command-line';
 
 const main = async () => {
-    const currentVersion = await execute('git describe --tags $(git rev-list --tags --max-count=1)');
-    const version = increaseVersionNumber(currentVersion.replace('\n', '').replace('v', ''));
+    const currentVersion = (await execute('git tag --sort=committerdate | tail -1'))
+        .replace('\n', '')
+        .replace('v', '');
+
+    const version = increaseVersionNumber(currentVersion);
+
+    console.log(`${currentVersion} -> ${version}`);
 
     await setVersionNumber(version);
     await execute(`git tag v${version} && git push origin --tags`);
-    await execute(`gh release create v${version} --title "Release v${version}" --notes ""`)
+    await execute(`gh release create v${version} --title "Release v${version}" --notes ""`);
 }
 
 main().catch(console.error);
