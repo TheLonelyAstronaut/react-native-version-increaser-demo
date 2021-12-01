735acc44c5d3ff2d6a99e80582ddcf231383ab4e
孤独な 宇宙飛行士 <50376577+TheLonelyAstronaut@users.noreply.github.com>
2021-12-01 14:10:06 +0300

    BREAKING CHANGE: changing app entry point logic.

--- App.tsx
+++ App.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import {Text, View} from "react-native";
 
 export const App = () => {
   console.log('BREAKING CHANGE');
-  return null;
+
+  return (
+      <View>
+        <Text>NO ITS BREAKING</Text>
+      </View>
+  );
 }
